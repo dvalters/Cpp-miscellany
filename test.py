147873e795c0f0828898c912579c9ae5d709246c
decvalts <declan.valters@manchester.ac.uk>
2015-09-12 20:07:31 +0100

    New sample tests

--- test.py
+++ test.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 15:11:26 2015
 
+Example usage of the imported C++ module.
+
+Using a simple Rectangle class.
+
+@author: dav
+"""
 
 import rectangle_wrapper
 
+# initialise a rectangle object with x0, y0, x1, y1 coords
+my_rectangle = rectangle_wrapper.PyRectangle(2,4,6,8)
 
+print my_rectangle.getLength()
+print my_rectangle.getHeight()
+print my_rectangle.getArea()
 
